2cdd24b504f12dee369c7f7289a0278c83e2ea3a
Carlos Cabral <carlosfc.631@gmail.com>
2020-12-21 20:44:50 -0300

    Proyecto Windows Form terminado

---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CRUDUsers.Models;
+
+namespace CRUDUsers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Refrescar();
+        }
+
+        #region Helper
+        private void Refrescar()
+        {
+            using (CRUDWFormEntities db = new CRUDWFormEntities())
+            {
+                var lst = from d in db.Users
+                          select d;
+
+                dataGridView1.DataSource = lst.ToList();
+            }
+        }
+
+        private int? GetId()
+        {
+            try
+            {
+                return int.Parse(dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[0].Value.ToString());
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Presentacion.FrmTable ofrmTable = new Presentacion.FrmTable();
+            ofrmTable.ShowDialog();
+
+            Refrescar();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int? id = GetId();
+
+            if (id != null)
+            {
+                Presentacion.FrmTable oFrmTable = new Presentacion.FrmTable(id);
+                oFrmTable.ShowDialog();
+
+                Refrescar();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int? id = GetId();
+
+            if (id != null)
+            {
+                using (CRUDWFormEntities db = new CRUDWFormEntities())
+                {
+                    Users oUsers = db.Users.Find(id);
+                    db.Users.Remove(oUsers);
+
+                    db.SaveChanges();
+                }
+
+                Refrescar();
+            }
+        }
+    }
+}
---
+++ Presentacion/FrmTable.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CRUDUsers.Models;
+
+namespace CRUDUsers.Presentacion
+{
+    public partial class FrmTable : Form
+    {
+        public int? id;
+        Users oUser = null;
+
+        public FrmTable(int? id = null)
+        {
+            InitializeComponent();
+
+            this.id = id;
+            if (id != null)
+            {
+                CargaDatos();
+            }
+        }
+
+        private void CargaDatos()
+        {
+            using (CRUDWFormEntities db = new CRUDWFormEntities())
+            {
+                oUser = db.Users.Find(id);
+                txtBoxName.Text = oUser.Nombre;
+                txtBoxEmail.Text = oUser.Correo;
+                dateTimePicker.Value = oUser.Fecha_Nacimiento;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using(CRUDWFormEntities db = new CRUDWFormEntities())
+            {
+                if(id == null)
+                {
+                    oUser = new Users();
+                }
+
+                oUser.Nombre = txtBoxName.Text;
+                oUser.Correo = txtBoxEmail.Text;
+                oUser.Fecha_Nacimiento = dateTimePicker.Value;
+
+                if(id == null)
+                    db.Users.Add(oUser);
+
+                else
+                {
+                    db.Entry(oUser).State = System.Data.Entity.EntityState.Modified;
+                }
+
+                db.SaveChanges();
+
+                this.Close();
+            }
+        }
+    }
+}
